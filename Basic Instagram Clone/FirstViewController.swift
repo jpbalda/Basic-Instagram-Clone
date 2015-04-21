99b14f6ab4b2cddee4b7e76f7106506d27126014
Juan Pablo Balda Andrade <jpbalda@JPBA-MBP.CASA>
2015-04-21 00:52:37 -0500

    Initial Commit

---
+++ Basic Instagram Clone/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Basic Instagram Clone
+//
+//  Created by Juan Pablo Balda Andrade on 4/21/15.
+//  Copyright (c) 2015 JPBA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Basic Instagram CloneTests/Basic_Instagram_CloneTests.swift
@@ -0,0 +1,36 @@
+//
+//  Basic_Instagram_CloneTests.swift
+//  Basic Instagram CloneTests
+//
+//  Created by Juan Pablo Balda Andrade on 4/21/15.
+//  Copyright (c) 2015 JPBA. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Basic_Instagram_CloneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
